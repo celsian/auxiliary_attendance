00b916086975fdf675b98bdfe56054dc01b1aacd
Lucas Paolella <celsian@gmail.com>
2014-09-26 14:14:50 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AuxiliaryAttendance::Application.config.secret_key_base = '9612b2c5a583c2dc05fa8e46f54c48d0e75590eecd9aa9a1e48385a1b5eb903810f8e93745819db74d4dc6f83777953b9e2e788ab9e65a249118e8116881c258'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AuxiliaryAttendance::Application.config.session_store :cookie_store, key: '_auxiliary_attendance_session'
