8785815a2d3819df603194fc0ceaab3119714f7e
Lucas Paolella <celsian@gmail.com>
2014-09-29 14:44:56 -0700

    Rails Generator Adds

---
+++ app/controllers/class_sessions_controller.rb
@@ -0,0 +1,41 @@
+class ClassSessionsController < ApplicationController
+  def index
+    if current_user
+      @class_sessions_open = ClassSession.where(user: current_user, closed: false)
+      @class_sessions_closed = ClassSession.where(user: current_user, closed: true)
+    end
+    # binding.pry
+  end
+
+  def show
+    @class_session = ClassSession.find(params[:id])
+  end
+
+  def new
+    @class_session = ClassSession.new
+  end
+
+  def create
+    @class_session = ClassSession.new(class_session_params)
+    @class_session.user = current_user
+
+    if @class_session.save
+      redirect_to class_session_path(@class_session), flash: {success: "Session was created."}
+    else
+      errors = ""
+      @class_session.errors.full_messages.each do |message|
+        errors += (" " + message + ".")
+      end
+
+      flash[:error] = "<B>ERROR:</B> #{errors}"
+      render :new
+    end
+  end
+
+  private
+
+  def class_session_params
+    params.require(:class_session).permit(:name)
+  end
+
+end
