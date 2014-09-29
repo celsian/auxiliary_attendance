3f657051f0d2b1a89ef20d7ec94208c4004086f4
Lucas Paolella <celsian@gmail.com>
2014-09-29 14:46:31 -0700

    Rails updates

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140927193551) do
+ActiveRecord::Schema.define(version: 20140929195103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "class_sessions", force: true do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.boolean  "closed",       default: false
+    t.string   "participants"
+    t.datetime "end_time"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "class_sessions", ["user_id"], name: "index_class_sessions_on_user_id", using: :btree
+
+  create_table "sessions", force: true do |t|
+    t.string   "name"
+    t.boolean  "closed",       default: false
+    t.string   "participants"
+    t.datetime "end_time"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "",    null: false
