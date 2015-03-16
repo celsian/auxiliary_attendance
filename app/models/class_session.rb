789688afaa7a744b7ee34745558570f02511804d
Lucas Paolella <celsian@gmail.com>
2015-03-16 15:22:11 -0700

    Class Session model creation

--- app/models/class_session.rb
+++ app/models/class_session.rb
@@ -1,5 +1,13 @@
 class ClassSession < ActiveRecord::Base
   belongs_to :user
+  has_many :class_session_students
+  has_many :students, through: :class_session_students
 
   validates :name, presence: true
+
+  def close
+    self.closed = true
+    self.end_time = Time.now
+    self.save
+  end
 end
