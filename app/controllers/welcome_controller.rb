ff1add6ee41c85d1b110db9f71d8949a66b44949
Lucas Paolella <celsian@gmail.com>
2015-03-16 15:20:59 -0700

    Check if current user belongs elsewhere

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,10 @@
 class WelcomeController < ApplicationController
   def index
+    if current_user
+      if current_user.teacher
+        redirect_to class_sessions_path
+      end
+    end
   end
   
 end
