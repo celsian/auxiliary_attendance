30da79bf06de0a1f27444e57754a86f3783a776c
Lucas Paolella <celsian@gmail.com>
2015-03-18 15:38:21 -0700

    Added resource routes for join table and admin
    Added post routes for class_session_students director, join session and
    leave session.

--- config/routes.rb
+++ config/routes.rb
@@ -1,61 +1,14 @@
 AuxiliaryAttendance::Application.routes.draw do
+  get "class_session_students/new"
   devise_for :users
   root 'welcome#index'
 
   resources :class_sessions
+  resources :class_session_students
+  resources :admin
   get "/class_sessions/:id/close_session", to: "class_sessions#close_session", as: "close_session"
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  post "/class_session_students/director", to: "class_session_students#director", as: "class_session_students_director"
+  post "/class_session_students/join_session", to: "class_session_students#join_session", as: "class_session_students_join_session"
+  post "/class_session_students/leave_session", to: "class_session_students#leave_session", as: "class_session_students_leave_session"
+  
 end
