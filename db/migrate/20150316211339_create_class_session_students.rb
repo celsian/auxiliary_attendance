ce78786572c593ca1975182260f2c45a8f06696b
Lucas Paolella <celsian@gmail.com>
2015-03-18 15:38:26 -0700

    Join Table Migration

--- db/migrate/20150316211339_create_class_session_students.rb
+++ db/migrate/20150316211339_create_class_session_students.rb
@@ -3,6 +3,10 @@ class CreateClassSessionStudents < ActiveRecord::Migration
     create_table :class_session_students do |t|
       t.belongs_to :class_session, index: true
       t.belongs_to :student, index: true
+      t.string :student_id_number
+      t.string :reason
+      t.datetime :start_time
+      t.datetime :end_time
 
       t.timestamps
     end
