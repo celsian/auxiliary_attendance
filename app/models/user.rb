d6a5d47392932e0046661f7d877c5ed28eb65080
Lucas Paolella <celsian@gmail.com>
2015-09-28 19:58:04 -0700

    Added User search and formatted_email
    Formatted email for extremely lengthy emails (>50 for now) may change
    formatted length late for mobile congruence.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,17 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :class_sessions
+
+  def self.search query
+    query = query.downcase
+    where("email LIKE :query", query: "%#{query}%")
+  end
+
+  def formatted_email
+    if email.length > 50
+      return email[0..(50-(email.length-email.index("@")))]+"..."+email[email.index("@")..-1]
+    else
+      return email
+    end
+  end
 end
