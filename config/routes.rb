f91534782258329be583c30a737d1a99853afe36
Lucas Paolella <celsian@gmail.com>
2015-09-28 19:59:07 -0700

    Routs for Admin and teacher adds/removes

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,17 @@ AuxiliaryAttendance::Application.routes.draw do
 
   resources :class_sessions
   # resources :class_session_students
-  resources :admin
-  
+
   get "/class_sessions/:id/close_session", to: "class_sessions#close_session", as: "close_session"
 
   post "/class_session_students/director", to: "class_session_students#director", as: "class_session_students_director"
   post "/class_session_students/join_session", to: "class_session_students#join_session", as: "class_session_students_join_session"
   post "/class_session_students/leave_session", to: "class_session_students#leave_session", as: "class_session_students_leave_session"
+
+  resources :admin, only: [:index]
+  get "/admin/teacher_editor", to: "admin#teacher_editor", as: "teacher_editor"
+  get "/admin/admin_editor", to: "admin#admin_editor", as: "admin_editor"
+  get "/admin/user/:id/add_teacher/:q", to: "admin#add_teacher", as: "add_teacher"
+  get "/admin/user/:id/remove_teacher/:q", to: "admin#remove_teacher", as: "remove_teacher"
 
 end
