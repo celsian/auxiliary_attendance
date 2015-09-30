6307d83c9b9244c1ce46f27a71bc517d8c30183c
Lucas Paolella <celsian@gmail.com>
2015-09-29 18:51:55 -0700

    Fleshed out the pagination code
    Added a constant for USERS_PER_PAGE
    Changed the search query’s return value to include the total result
    count and limit the returned results to 10.
    Added a User.page_index function for result limiting.
    Added pagination methods for teachers as well (much less complex)
    
    In other news, default_scope is set to email asc

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,26 @@ class User < ActiveRecord::Base
 
   has_many :class_sessions
 
-  def self.search query
+  default_scope { order("email ASC") }
+
+  USERS_PER_PAGE = 10.0
+
+  def self.search query, page_index
     query = query.downcase
-    where("email LIKE :query", query: "%#{query}%")
+    result = where("email LIKE :query", query: "%#{query}%")
+    result_count = result.count
+    result = result[(User.page page_index)..-1]
+    return [result_count, result[0..USERS_PER_PAGE-1]]
+  end
+
+  def self.page page_index
+    USERS_PER_PAGE*page_index.to_i
+  end
+
+  def self.teachers page_index
+    teachers = User.where(teacher: true)
+    teachers = teachers[(User.page page_index)..-1]
+    teachers[0..USERS_PER_PAGE-1]
   end
 
   def formatted_email
@@ -17,5 +34,25 @@ class User < ActiveRecord::Base
     else
       return email
     end
+  end
+
+  def self.teacher_count
+    return User.where(teacher: true).count
+  end
+
+  # def self.admin_count
+  #   return User.where(admin: true).count
+  # end
+
+  def self.user_pages user_count
+    (user_count/USERS_PER_PAGE).ceil
+  end
+
+  def self.teacher_pages
+    (User.teacher_count/USERS_PER_PAGE).ceil
+  end
+
+  def self.admin_pages
+    (User.admin_count/USERS_PER_PAGE).ceil
   end
 end
