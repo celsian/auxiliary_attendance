dbf390f9b8e721bec1c90e54a6495e5de19360b2
Lucas Paolella <celsian@gmail.com>
2015-10-01 16:43:48 -0700

    Route changes for import.

--- config/routes.rb
+++ config/routes.rb
@@ -14,8 +14,11 @@ AuxiliaryAttendance::Application.routes.draw do
 
   resources :admin, only: [:index]
   get "/admin/teacher_editor", to: "admin#teacher_editor", as: "teacher_editor"
-  get "/admin/admin_editor", to: "admin#admin_editor", as: "admin_editor"
   get "/admin/user/:id/add_teacher/:q", to: "admin#add_teacher", as: "add_teacher"
   get "/admin/user/:id/remove_teacher/:q", to: "admin#remove_teacher", as: "remove_teacher"
+  get "/admin/admin_editor", to: "admin#admin_editor", as: "admin_editor"
+  get "/admin/user/:id/add_admin/:q", to: "admin#add_admin", as: "add_admin"
+  get "/admin/user/:id/remove_admin/:q", to: "admin#remove_admin", as: "remove_admin"
+  get "/admin/import", to: "admin#import", as: "import"
 
 end
