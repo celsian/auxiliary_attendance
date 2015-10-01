43dcb0c3dd31a406206902c49c1189976d57006e
Lucas Paolella <celsian@gmail.com>
2015-10-01 16:37:48 -0700

    Multiple updates
    Changed render locations for errors.
    Added Admin editor actions.
    Added import action (untested)

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -7,12 +7,14 @@ class AdminController < ApplicationController
     if params[:q] && params[:q].blank? || !params[:q]
       params[:q] = "@"
     end
+
     results = User.search(params[:q], params[:s])
     @users = results.last
     @user_pages = User.user_pages(results.first)
 
     @teachers = User.teachers(params[:t])
     @teacher_pages = User.teacher_pages
+
   end  
 
   def add_teacher
@@ -22,7 +24,7 @@ class AdminController < ApplicationController
       redirect_to teacher_editor_path(q: params[:q]), flash: { success: "#{user.email} is now a Teacher." }
     else
       flash[:error] = "Error: There was a problem adding the rights."
-      render :add_teacher
+      render :teacher_editor
     end
   end
 
@@ -33,11 +35,50 @@ class AdminController < ApplicationController
       redirect_to teacher_editor_path(q: params[:q]), flash: { success: "#{user.email} is no longer a Teacher." }
     else
       flash[:error] = "Error: There was a problem adding the rights."
-      render :add_teacher
+      render :teacher_editor
     end
   end
 
   def admin_editor
+    if params[:q] && params[:q].blank? || !params[:q]
+      params[:q] = "@"
+    end
+    results = User.search(params[:q], params[:s])
+    @users = results.last
+    @user_pages = User.user_pages(results.first)
 
+    @admins = User.admins(params[:a])
+    @admin_pages = User.admin_pages
   end
+
+  def add_admin
+    user = User.find(params[:id])
+    user.admin = true
+
+    if user.save
+      redirect_to admin_editor_path(q: params[:q]), flash: { success: "#{user.email} is now an Administrator." }
+    else
+      flash[:error] = "Error: There was a problem adding the rights."
+      render :admin_editor
+    end
+  end
+
+  def remove_admin
+    user = User.find(params[:id])
+    user.admin = false
+    if user.save
+      redirect_to admin_editor_path(q: params[:q]), flash: { success: "#{user.email} is no longer an Administrator." }
+    else
+      flash[:error] = "Error: There was a problem adding the rights."
+      render :admin_editor
+    end
+  end
+
+  def import
+    if params[:file]
+      Student.import(params[:file])
+      redirect_to import_path, flash: {success: "Students & Devices imported."}
+    end
+  end
+
 end
