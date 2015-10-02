42989ff1b81b6d1e1a9c0ad72080e3ad44d80376
Lucas Paolella <celsian@gmail.com>
2015-10-01 18:05:10 -0700

    Added redirect for Administrators who don't have access to personal class sessions.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,8 @@ class WelcomeController < ApplicationController
     if current_user
       if current_user.teacher
         redirect_to class_sessions_path
+      elsif current_user.admin
+        redirect_to admin_index_path
       end
     end
   end
