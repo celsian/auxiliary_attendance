66ff1d35f0a41ead381be27496c6dc36a138c7c6
Lucas Paolella <celsian@gmail.com>
2015-10-05 20:05:03 -0700

    rails adds

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150316211339) do
+ActiveRecord::Schema.define(version: 20151006013956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20150316211339) do
     t.string   "last_name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "enabled",    default: true
   end
 
   create_table "users", force: true do |t|
