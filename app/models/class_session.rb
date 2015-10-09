9edc610a8dddff2a38515e1f8266028c3e33ce96
Lucas Paolella <celsian@gmail.com>
2015-10-08 18:57:05 -0700

    Added a check for stale classes (classes open for more than 9 hours)
    Specifics are as follows:
    Classes open more than 9 hours are automatically closed.
    If the class has students who have signed out the classes end time will
    be adjusted to the final students departure time, or the final
    underrated students start time.
    If the class has students who did not sign out their time will be
    adjusted to the last departing student time or the last sign in time,
    whichever is most recent.
    If the class had no students the close time will be adjusted to the
    open time, negating the class.
    If the class had students but none signed out, all departure times for
    students and class close time will be set to the last students signing
    time.

--- app/models/class_session.rb
+++ app/models/class_session.rb
@@ -10,4 +10,34 @@ class ClassSession < ActiveRecord::Base
     self.end_time = Time.now
     self.save
   end
+
+  def self.stale_classes?
+    stale_classes = ClassSession.where("closed = ? AND created_at < ?", false, 9.hours.ago)
+
+    stale_classes.each do |class_session|
+      class_session.auto_close
+    end
+  end
+
+  def auto_close
+    time = created_at #Initializing this incase no students signed out. (Should be when the last student left)
+
+    class_session_students.each do |css|
+      if css.start_time && css.start_time > time
+        time = css.stat_time
+      end
+      if css.end_time && css.end_time > time
+        time = css.end_time
+      end
+    end
+
+    class_session_students.where(end_time: nil).each do |class_session_student|
+      class_session_student.end_time = time
+      class_session_student.save
+    end
+
+    self.closed = true
+    self.end_time = time
+    self.save
+  end
 end
