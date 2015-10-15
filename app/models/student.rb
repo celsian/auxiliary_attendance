6a99864373c8fccef6fcdebec39f08172e57f278
Lucas Paolella <celsian@gmail.com>
2015-10-14 18:51:07 -0700

    New methods for calculating various things about a calendar month.

--- app/models/student.rb
+++ app/models/student.rb
@@ -17,6 +17,7 @@ class Student < ActiveRecord::Base
     messages
   end
 
+######Calendar Calculations#######
   def class_session_timeline
     all_months = []
 
@@ -38,13 +39,46 @@ class Student < ActiveRecord::Base
     return nil
   end
 
-  def self.calendar_start day
-    day-(day.wday).days
+  def self.calendar month
+    results = {}
+    results[:month_start] = calendar_start month
+    results[:month_end] = calendar_end month
+    results
   end
 
-  def self.calendar_end day
-    day+(6-day.wday).days
+  # def self.calendar_weeks month
+  #   results = calendar month
+  #   ((results[:month_end] - results[:month_start])/(24 * 60 * 60)).ceil/7
+  # end
+
+  def self.calendar_weeks_start month
+    results = Student.calendar month
+    current_week = results[:month_start]
+    weeks = []
+
+    while current_week.to_s != (results[:month_end]+1.second).to_s
+      weeks << current_week
+      current_week+=7.days
+    end
+
+    weeks
   end
+
+  def self.calendar_start month
+    month_start = month.at_beginning_of_month
+    month_start-(month_start.wday).days
+  end
+
+  def self.calendar_end month
+    month_end = month.at_end_of_month
+    month_end+(6-month_end.wday).days
+  end
+
+  def class_sessions_for_month_results month
+    results = Student.calendar month
+    class_session_students.where(start_time: results[:month_start]..results[:month_end])
+  end
+#####End Calendar Calculations########
 
   def self.search query, page_index, enabled_bool
     query = query.downcase
