a07c8c097c1c8f79e25f66c6a70d126b1034396d
Lucas Paolella <celsian@gmail.com>
2015-10-21 17:51:00 -0700

    Added the calendar calculations necessary for reporting stats

--- app/models/user.rb
+++ app/models/user.rb
@@ -69,4 +69,62 @@ class User < ActiveRecord::Base
   def self.admin_pages
     (User.admin_count/USERS_PER_PAGE).ceil
   end
+
+  ######Calendar Calculations#######
+  def class_session_timeline
+    all_months = []
+
+    class_sessions_results = class_sessions.reorder(created_at: :asc)
+    if class_sessions_results.length > 0
+      start_month = class_sessions_results.first.created_at.at_beginning_of_month
+      end_month = class_sessions_results.last.created_at.at_beginning_of_month
+
+      unless end_month == start_month
+        all_months << end_month
+        end_month = end_month-1.month
+      end
+
+      all_months << start_month
+
+      return all_months
+    end
+
+    return nil
+  end
+
+  def self.calendar month
+    results = {}
+    results[:month_start] = calendar_start month
+    results[:month_end] = calendar_end month
+    results
+  end
+
+  def self.calendar_weeks_start month
+    results = User.calendar month
+    current_week = results[:month_start]
+    weeks = []
+
+    while current_week.to_s != (results[:month_end]+1.second).to_s
+      weeks << current_week
+      current_week+=7.days
+    end
+
+    weeks
+  end
+
+  def self.calendar_start month
+    month_start = month.at_beginning_of_month
+    month_start-(month_start.wday).days
+  end
+
+  def self.calendar_end month
+    month_end = month.at_end_of_month
+    month_end+(6-month_end.wday).days
+  end
+
+  def class_sessions_for_month_results month
+    results = User.calendar month
+    class_session.where(start_time: results[:month_start]..results[:month_end])
+  end
+#####End Calendar Calculations########
 end
