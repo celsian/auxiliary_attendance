0bf9b16999d5adb68b9a151945477ce3676b5d7c
Lucas Paolella <celsian@gmail.com>
2016-06-06 12:40:22 -0700

    Added previous and next day links

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -4,6 +4,17 @@ class StatsController < ApplicationController
   end
 
   def general
-    @class_sessions = ClassSession.where("created_at >= ?", Time.zone.now.beginning_of_day)
+    offset = 0
+
+    if params[:offset]
+      offset = params[:offset].to_i
+    end
+    
+    @class_sessions = ClassSession.where("created_at >= :day_start AND created_at <= :day_end", 
+                                          { 
+                                            day_start: (Time.zone.now+offset.days).beginning_of_day,
+                                            day_end: (Time.zone.now+offset.days).end_of_day
+                                          }
+                                        )
   end
 end
