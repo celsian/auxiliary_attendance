826177b7e10f0bba52a06687279894679c0cf8a5
Lucas Paolella <celsian@gmail.com>
2016-07-06 12:40:47 -0700

    refactor auto-close method

--- app/models/class_session.rb
+++ app/models/class_session.rb
@@ -28,18 +28,15 @@ class ClassSession < ActiveRecord::Base
   def auto_close
     time = created_at #Initializing this incase no students signed out. (Should be when the last student left)
 
-    class_session_students.each do |css|
-      if css.start_time && css.start_time > time
-        time = css.start_time
-      end
-      if css.end_time && css.end_time > time
-        time = css.end_time
-      end
-    end
-
     class_session_students.where(end_time: nil).each do |class_session_student|
       class_session_student.end_time = class_session_student.start_time
       class_session_student.save
+    end
+
+    class_session_students.each do |css|
+      if css.end_time && css.end_time > time
+        time = css.end_time
+      end
     end
 
     self.closed = true
