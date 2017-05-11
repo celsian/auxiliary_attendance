a9efc8618b5a93dc6cad5e2540576e1e43412e5e
Lucas Paolella <celsian@gmail.com>
2017-05-11 08:31:33 -0700

    Revert changes, flash notice was popping up on other pages, not good

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,7 @@
 class WelcomeController < ApplicationController
   def index
     if current_user
+      ClassSession.active_class_count > 0 ? flash[:info] = Welcome.count_notification : ""
       if current_user.teacher
         redirect_to class_sessions_path
       elsif current_user.admin
--- app/models/welcome.rb
+++ app/models/welcome.rb
@@ -1,2 +1,12 @@
 class Welcome
+  def self.count_notification
+    active_class_count = ClassSession.active_class_count
+    active_student_count = ClassSessionStudent.active_student_count
+
+    unless active_class_count == 0 && active_student_count == 0
+      return "Welcome! There are currently #{active_student_count} #{'student'.pluralize(active_student_count)} in #{active_class_count} #{'class'.pluralize(active_class_count)}."
+    else
+      return ""
+    end
+  end
 end
